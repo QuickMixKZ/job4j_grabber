b7a99d531ca18068f250c7055ef49f960efdb659
quickmixkz@gmail.com <QuickMixKZ>
2022-05-10 15:32:26 +0500

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,15 +5,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.models.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=P", SOURCE_LINK);
+
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     private String retrieveDescription(String link) throws IOException {
         Connection connection = Jsoup.connect(link);
@@ -22,9 +32,11 @@ public class HabrCareerParse {
         return description.first().text();
     }
 
-    public static void main(String[] args) throws IOException {
-        for (int page = 1; page <= 5; page++) {
-            Connection connection = Jsoup.connect(PAGE_LINK.replace('P', (char) page));
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> result = new ArrayList<>();
+        for (int page = 1; page <= 20; page++) {
+            Connection connection = Jsoup.connect(link.replace('P', (char) page));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
@@ -32,9 +44,17 @@ public class HabrCareerParse {
                 Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
                 String date = row.select(".vacancy-card__date").first().child(0).attr("datetime");
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, date);
+                String vacancyLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String description = "";
+                try {
+                    description = retrieveDescription(vacancyLink);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                LocalDateTime created = dateTimeParser.parse(date);
+                result.add(new Post(0, vacancyName, vacancyLink, description, created));
             });
         }
+        return result;
     }
 }
