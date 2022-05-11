bd55d00edc5bc25b104dd180757da60d0af0ff74
quickmixkz@gmail.com <QuickMixKZ>
2022-05-11 12:24:53 +0500

    2.4. SqlRuParse [#285213 #299351]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -17,8 +17,6 @@ public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
-
     private final DateTimeParser dateTimeParser;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
