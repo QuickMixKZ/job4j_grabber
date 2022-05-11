4f7628a345ba9ccf430cfe7adbf0707248da68f4
quickmixkz@gmail.com <QuickMixKZ>
2022-05-11 11:23:35 +0500

    2.2. Модель данных - Post. [#285211 #299285]

--- src/main/java/ru/job4j/grabber/models/Post.java
+++ src/main/java/ru/job4j/grabber/models/Post.java
@@ -10,6 +10,61 @@ public class Post {
     private String description;
     private LocalDateTime created;
 
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
     @Override
     public String toString() {
         return "Post{"
@@ -30,20 +85,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(link, post.link)
-                && Objects.equals(created, post.created);
+        return id == post.id
+                && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(link, created);
-    }
-
-    public Post(int id, String title, String link, String description, LocalDateTime created) {
-        this.id = id;
-        this.title = title;
-        this.link = link;
-        this.description = description;
-        this.created = created;
+        return Objects.hash(id, link);
     }
 }
