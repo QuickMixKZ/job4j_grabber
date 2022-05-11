cbcde6d647c7d96b14f54b188607bb2c226096f2
quickmixkz@gmail.com <QuickMixKZ>
2022-05-11 15:40:37 +0500

    6. Grabber. [#289477 #299709]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -10,6 +10,9 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.awt.*;
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -18,6 +21,7 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
+    private static final String LINK = "https://career.habr.com/vacancies/java_developer";
 
     public Store store() {
         return new PsqlStore(cfg);
@@ -60,9 +64,30 @@ public class Grabber implements Grab {
             JobDataMap map = context.getJobDetail().getJobDataMap();
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
-            java.util.List<Post> result = parse.list("https://career.habr.com/vacancies/java_developer");
+            java.util.List<Post> result = parse.list(LINK);
             result.forEach(store::save);
         }
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write((post.toString().getBytes(Charset.forName("Windows-1251"))));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 
@@ -72,5 +97,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+        grab.web(store);
     }
 }
